28b19d01ae4a1614f52530069f48e09fefbd1876
1738kimutai <davieskimutai4@gmail>
2023-02-12 17:08:48 +0300

    done

---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person
+    def talk
+        puts "Hello World!"
+    end
+    def walk
+      puts "The Person is walking"
+    end
+end
+
+# Davis = Person.new
+# Davis.talk
+# Davis.walk
+# Davis.run
